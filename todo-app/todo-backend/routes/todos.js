4613ea726ae07f2442fed2f90cba8879d1326c62
Juha Järvensivu <juha.matti.jarvensivu@gmail.com>
2023-09-13 07:41:45 +0300

    add exercise 12.7.

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,17 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo)
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const { text, done } = req.body
+  const { todo } = req
+  todo.text = text
+  todo.done = done
+  const savedTodo = await todo.save()
+  res.send(savedTodo)
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
